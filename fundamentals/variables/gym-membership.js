41b2db5f6c72081b5611d7412ea8c3271af3c630
reivaj93 <jav.alonso0493@gmail.com>
2025-03-11 19:58:25 -0600

    chore(variables): add gym-membership exercise

---
+++ fundamentals/variables/gym-membership.js
@@ -0,0 +1,21 @@
+/* El elite Fitness Center es un gimnasio boutique ubicado en Pal;o Alto, California,
+ofrece membresias por $150 dolares al mes.
+Opera de 6 am a 10 pm, emplea a 10 entrenadores certificados y cuenta con comodidades como sauna, piscina y clases grupales. */
+
+const gymName = 'Elite Fitness Center';
+
+const gymLocation = 'Palo Alto, California';
+
+const openHour = '6am';
+
+const closeHour = '10pm';
+
+const gymSchedule = '6 am to 10 pm';
+
+const gymMembershipPrice = '$150';
+
+const gymServices = ['sauna', 'piscina', 'clases grupales'];
+
+console.log(`El ${gymName}se encuentra en ${gymLocation} ,tiene una membresia mensual de ${gymMembershipPrice}.\n
+Opera desde las ${openHour} hasta las ${closeHour}.\n
+Ofrece los siguientes servicios ${gymServices}`) 
