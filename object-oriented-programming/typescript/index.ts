ccfc33eab1603d67af139e4ace1a3c4c7da318b5
reivaj93 <jav.alonso0493@gmail.com>
2025-04-15 19:41:52 -0600

    feat(typescript): migrate objects folder files to ts extension

--- object-oriented-programming/typescript/index.ts
+++ object-oriented-programming/typescript/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello, TypeScript!");
+const message: string  = 'Hello, World!';
+console.log(message);
+
+interface Account{
+
+    id: number;
+    name: string;
+    balance: number;
+    deposit: (amount: number) => void;
+}
+
+const createAccount = (id: number, name: string, balance:number = 0): Account => {
+
+    return { 
+        id, 
+        name, 
+        balance ,
+        deposit: (amount: number) => {
+            console.log(`Depositing ${amount} to account ${id}`);
+
+        }
+
+    }
+    }
+
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
+
+const result = prompt('message: '); 
+
+console.log(result)
+
