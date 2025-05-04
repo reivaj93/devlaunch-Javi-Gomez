ac7a124dcd11bbcc9d20d9877bc60d6fdb953f00
reivaj93 <jav.alonso0493@gmail.com>
2025-05-03 19:50:09 -0600

    feat(UML): add explanation

---
+++ object-oriented-programming/UML.ts
@@ -0,0 +1,9 @@
+/*
+Relaciones
+
+Siempre permea el mayor
+
+1:1 - 1:1 = 1:1  (relaciones 1 a 1 tipo 1 persona solo tiene un pasarpote y viceversa.)
+1:n - 1:1 = 1:n (relaciones 1 a muchos tipo 1 autor puede tener muchos libros, pero el libro solo puede tener un autor.)
+1:n = n:1 = n:n (relaciones muchos a muchos tipo un estudiante puede tener muchos cursos y un curso puede tener muchos estudiantes.)
+*/
