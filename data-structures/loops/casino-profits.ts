e62fced1a962e56a1c32bc3ec4a3d4c78a49c483
reivaj93 <jav.alonso0493@gmail.com>
2025-05-05 14:25:43 -0600

    feat(loops): add folder exercise

---
+++ data-structures/loops/casino-profits.ts
@@ -0,0 +1,18 @@
+/*
+
+En un casino en linea han implementado un nuevo juego, que consite en lanzar tres dados y anotar sus valores.
+Si la suma de estos valores es mayor o igual a 10, el apostador gana 10$ de lo contrario pierde 15$.
+
+Para probar el programa a fondo, simularas 50 intentos de personas jugando, cada jugador simula un intento.
+
+Despues de que todos los jugadores hayan terminado sus intentos, encuentra el resultado de las ganacias del casino empezando en 0.
+
+el resultado puede ser tanto positivo como negativo, donde negativo representa
+que el casino perdio dinero ya que los jugadores ganaron mas dinero del que perdieron.
+
+Si pruebas varias veces el codigo puedes topar con diferentes resultados como: 100, 150, -50,-25,75
+
+Pero mayormente dara numeros positivos que negativos en ganancias porque recuerda:
+"La casa (casino) nunca pierde.."
+
+*/
---
+++ data-structures/loops/tarot-number.ts
@@ -0,0 +1,23 @@
+/* 
+En el Tarot, se usa la numerologia para entender mejor a una persona. Para encontrar su numero de Tarot, observamos su fecha de nacimiento. 
+Al sumar los digitos de su fecha de nacimiento, podemos encontrar un numero especial que los representa.
+
+Para empezar, pensemos en alguien nacido el 28 de noviembre de 1989
+Para descubrir el numero, debemos seguir varios pasos:
+
+SUmamos los numeros de su fecha de nacimiento : el dia, el mes, el anho.
+Entonces, sumamos 28 + 11+ 1989, obteniendo 2028.
+
+Ahora, descomponemos este numero grande en partes mas pequenhas.
+Asi que para 2028, sumamos 2+0+2+8, lo cual da 12.
+
+Finalmente, simplificamos este numero aun mas. Para 12, sumamos 1+2, lo cual de 3.
+Ese es el numero especial que estamos buscando.
+
+Recuerda utilizar bucles para reduciar al maximo los calculos numericos realizados para valida que la fecha ingresada sea correcta, 
+no puedes recibir una fecha como 50/50/2020
+
+Aunque podrias recibir una como 12/12/10000 -> 7, ya que podria ser alguien del futuro usando tu codigo.
+
+*/
+
